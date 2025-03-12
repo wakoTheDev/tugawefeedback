b3c1f82a0680b78407539109c14859c605f2c713
wakoTheDev <wakopaul19@gmail.com>
2025-03-12 14:17:56 +0300

    port changes

--- feedbacksystem.py
+++ feedbacksystem.py
@@ -165,5 +165,5 @@ def home():
 
 
 if __name__ == "__main__":
-    uvicorn.run("feedbacksytem:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("feedbacksytem:app", host="0.0.0.0", port=8000, log_level="info")
     
