137474cb397a88399bed3fca64a94a4e448d7376
wakoTheDev <162966971+wakoTheDev@users.noreply.github.com>
2025-03-16 11:23:14 +0300

    Update feedbacksystem.py

--- feedbacksystem.py
+++ feedbacksystem.py
@@ -5,6 +5,7 @@ import requests
 import httpx
 import uvicorn
 import os
+import base64
 
 # SQLAlchemy imports for database handling
 from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
